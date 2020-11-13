5a25aa9908654e93f4dd39615f5d97ca017893b7
alijdens <axel.lijdens@epson.com.ar>
2020-11-13 01:53:49 -0300

    exercise 9 done

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,9 +1,15 @@
 (ns exercise9)
+(require '[clojure.string :as str])
 
 (defn word-count 
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (frequencies
+    (filter
+      #(> (count %) 0)
+      (str/split (str/lower-case x) #"[^\w]")
+    )
+  )
 )
 
 
