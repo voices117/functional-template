3087bd7da718c99eb0f7ef1bf3d83026fe259ecf
alijdens <axel.lijdens@epson.com.ar>
2020-11-13 00:13:03 -0300

    exercise 4 done

--- src/exercise4.clj
+++ src/exercise4.clj
@@ -3,7 +3,7 @@
 (defn summary
   "Calculates the sum of all the values in the sequence of numbers given as input. Use the reduce function."
   [x]
-  (throw (Exception. "Not implemented."))
+  (reduce + x)
 )
 
 
