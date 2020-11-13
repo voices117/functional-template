dd6be4abec61768f56d7b55ae86f4818d7ed2cec
alijdens <axel.lijdens@epson.com.ar>
2020-11-13 00:11:13 -0300

    exercise 2 done

--- src/exercise2.clj
+++ src/exercise2.clj
@@ -3,7 +3,7 @@
 (defn only-greater-than-five
   "Filters the input list returning only the elements greater than 5."
   [x]
-  (throw (Exception. "Not implemented."))
+  (filter (partial < 5) x)
 )
 
 
